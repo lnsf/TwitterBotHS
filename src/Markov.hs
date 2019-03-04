d4702fa698082501c63ce017a4de55ef372725f7
lnsf <lnsf03@gmail.com>
2019-03-04 21:09:32 +0900

    :sparkles: add create func

--- src/Markov.hs
+++ src/Markov.hs
@@ -1,1 +1,12 @@
-module Markov where
+module Markov
+  ( createBlocks
+  )
+where
+
+import           Text.MeCab
+import           Data.List.Split
+
+createBlocks :: String -> IO [String]
+createBlocks s = do
+  mcb <- new2 ""
+  map nodeSurface <$> parseToNodes mcb s
