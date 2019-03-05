a4ca79e26aa50cb994873436a1bf1c1cb38fdd5b
lnsf <lnsf03@gmail.com>
2019-03-05 22:20:51 +0900

    :+1: Add remove func for unneeds

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,15 @@
 module Lib
-    ( someFunc
-    ) where
+  ( rmvUnneeds
+  )
+where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Data.List                     as L
+import qualified Data.Text                     as T
+
+rmvUnneeds :: [T.Text] -> [T.Text]
+rmvUnneeds []         = []
+rmvUnneeds (tw : tws) = [ tw | ok tw ] ++ rmvUnneeds tws
+ where
+  ok :: T.Text -> Bool
+  ok tw =
+    L.all (\p -> not (T.isInfixOf (T.pack p) tw)) ["http", "RT @", "@", "#"]
