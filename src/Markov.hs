07e42407a700e8d2338eca9cf0c85ccd24ba0833
lnsf <lnsf03@gmail.com>
2019-03-06 17:24:48 +0900

    :construction: Add functions for tokenize and create blocks

--- src/Markov.hs
+++ src/Markov.hs
@@ -1,12 +1,21 @@
 module Markov
   ( createBlocks
+  , tokenize
   )
 where
 
 import           Text.MeCab
 import           Data.List.Split
+import qualified Data.Text                     as T
 
-createBlocks :: String -> IO [String]
-createBlocks s = do
+type Block = (T.Text, T.Text, T.Text)
+
+tokenize :: T.Text -> IO [T.Text]
+tokenize s = do
   mcb <- new2 ""
   map nodeSurface <$> parseToNodes mcb s
+
+createBlocks :: [T.Text] -> [Block]
+createBlocks [f, s]           = []
+createBlocks (f : s : t : ws) = (f, s, t) : createBlocks (s : t : ws)
+
