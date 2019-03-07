981bce43a9c5f1851046531d60e15a37c60a9722
lnsf <lnsf03@gmail.com>
2019-03-07 15:06:16 +0900

    :+1: Add function for formatting blocks and put spaces for english

--- src/Markov.hs
+++ src/Markov.hs
@@ -2,6 +2,8 @@ module Markov
   ( createBlocks
   , tokenize
   , connectBlocks
+  , toText
+  , Block
   )
 where
 
@@ -16,7 +18,20 @@ type Block = (T.Text, T.Text, T.Text)
 tokenize :: T.Text -> IO [T.Text]
 tokenize s = do
   mcb <- new2 ""
-  map nodeSurface <$> parseToNodes mcb s
+  ws  <- map nodeSurface <$> parseToNodes mcb s
+  return $ addSpace ws
+ where
+  -- for english vocablary
+  addSpace :: [T.Text] -> [T.Text]
+  addSpace []             = []
+  addSpace [w           ] = [w]
+  addSpace (w1 : w2 : ws) = if onlyAlp w1 w2
+    then w1 : T.pack " " : addSpace (w2 : ws)
+    else w1 : addSpace (w2 : ws)
+  onlyAlp :: T.Text -> T.Text -> Bool
+  onlyAlp w1 w2 =
+    not (T.null w1 || T.null w2) && all (T.all (`elem` ['A' .. 'z'])) [w1, w2]
+
 
 createBlocks :: [T.Text] -> [Block]
 createBlocks [f, s]           = []
@@ -27,8 +42,6 @@ connectBlocks b bs = do
   next <- getNext b bs
   return $ b : next
  where
-  bfst (f, _, _) = f
-  blst (_, _, l) = l
   cancon b bs = [ x | x <- bs, bfst x == blst b ]
   getNext b bs = if null $ cancon b bs
     then return []
@@ -42,3 +55,10 @@ takeRdm [] = error "Empty list"
 takeRdm xs = do
   idx <- R.getStdRandom (R.randomR (0, length xs - 1))
   return $ xs !! idx
+
+toText :: [Block] -> T.Text
+toText = foldr (\b -> T.append (bfst b `T.append` bsnd b)) T.empty
+
+bfst (f, _, _) = f
+bsnd (_, s, _) = s
+blst (_, _, l) = l
