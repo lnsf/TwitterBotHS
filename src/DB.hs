a8794b0121fe8b375187d469be9c738e8ba19949
lnsf <lnsf03@gmail.com>
2019-03-13 17:53:12 +0900

    :boom: change data type

--- src/DB.hs
+++ src/DB.hs
@@ -2,9 +2,11 @@
 
 module DB (createConnection, addToDB, readDB, deleteById, Word) where
 
-import           Types
+import Lib
+import           Data.Block
 import           GHC.Generics
 import           Database.PostgreSQL.Simple
+import Prelude hiding (id)
 import qualified Data.Text as T
 
 createConnection :: IO Connection
@@ -14,15 +16,19 @@ createConnection = connect
                        , connectPassword = "postgres"
                        }
 
-addToDB :: Connection -> DBEntry -> IO ()
-addToDB con (a, b, c, i) = do
-  execute con "INSERT INTO words VALUES (?, ?, ?, ?)" (a, b, c, i)
+addToDB :: Connection -> Block -> IO ()
+addToDB con b = do
+  execute con "INSERT INTO words VALUES (?, ?, ?, ?)" ((get1 . block) b, (get2 . block) b, (get3 . block) b, id b)
   return ()
 
-readDB :: Connection -> IO [DBEntry]
-readDB con = query_ con "SELECT * FROM words"
+readDB :: Connection -> IO [Block]
+readDB con = toBlock <$> query_ con "SELECT * FROM words"
+  where
+    toBlock :: [(T.Text, T.Text, T.Text, Integer)] -> [Block]
+    toBlock [] = []
+    toBlock ((a, b, c, i):xs) = Block (a, b, c) i : toBlock xs
 
-deleteById :: Connection -> Int -> IO ()
+deleteById :: Connection -> Integer -> IO ()
 deleteById con id_ = do
   execute con "DELETE FROM words WHERE id_ = ?" [id_]
   return ()
