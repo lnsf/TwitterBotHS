629fece6f2d5a9c6dd819b1a3268f613178e0c58
lnsf <lnsf03@gmail.com>
2019-04-07 14:06:25 +0900

    :bug: Always True...

--- src/Data/Block.hs
+++ src/Data/Block.hs
@@ -7,7 +7,7 @@ data Block = Block { block :: (T.Text, T.Text, T.Text), id :: Integer }
   deriving Show
 
 instance Eq Block where
-  (Block (a, b, c) _) == (Block (a', b', c') _) = a == a && b == b && c == c
+  (Block (a, b, c) _) == (Block (a', b', c') _) = a == a' && b == b' && c == c'
 
 newBlock :: (T.Text, T.Text, T.Text) -> Integer -> Block
 newBlock = Block
