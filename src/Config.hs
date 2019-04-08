55bacc646fedc12d9e56667c839876b52d1d16a8
lnsf <lnsf03@gmail.com>
2019-04-08 22:16:11 +0900

    :boom: Put config.yaml in the same directory as executable files.

--- src/Config.hs
+++ src/Config.hs
@@ -3,7 +3,9 @@
 module Config where
 
 import           GHC.Generics
+import           System.Environment
 import qualified Data.Yaml as Y
+import Data.List
 
 data Config = Config { name :: String, keys :: Keys }
   deriving (Show, Generic)
@@ -16,4 +18,6 @@ instance Y.FromJSON Config
 instance Y.FromJSON Keys
 
 getConfig :: IO Config
-getConfig = either (error . show) id <$> Y.decodeFileEither "./config.yaml"
+getConfig = do
+  p <- dropWhileEnd (/= '/') <$> getExecutablePath
+  either (error . show) id <$> Y.decodeFileEither (p ++ "config.yaml")
